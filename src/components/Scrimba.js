c1b4a2fb970fde5eced0fd125b654a42cf8774f8
Svetlana <83650741+Svmar1203@users.noreply.github.com>
2022-12-20 21:28:32 +0200

    20.12.22

--- src/components/Scrimba.js
+++ src/components/Scrimba.js
@@ -1,11 +1,22 @@
 function Scrimba() {
   return (
-    <div>
+    <div className="scrimba">
       <div className="barnav">
-        <p>Navbar goes here</p>
+        <img
+          src="https://blog.logrocket.com/wp-content/uploads/2022/01/loading-state-react.png"
+          alt="react-logo"
+        />
+        <h3>ReactFacts</h3>
+        <h4>React Course - Project 1</h4>
       </div>
       <div className="mainn">
-        <p>Inter</p>
+        <h1>Fun facts about React</h1>
+        <ul>
+          <li>Was first released in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has well over 100K stars on GitHub</li>
+          <li>Is maintained by Facebook</li>
+        </ul>
       </div>
     </div>
   );
